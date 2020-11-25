2edb129e14cb018f378c136e55a4480ed03fe570
elijahstr <elijahstraight14@gmail.com>
2020-11-25 11:27:08 -0700

    server fix

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ app.post('/auth/logout', adminCtrl.logout);
 
 app.post('/api/email', emailCtrl.email);
 
-app.use(express.static(__dirname + '../build'))
+app.use(express.static(__dirname + '/../build'))
 app.get('*', (req, res) =>{
     res.sendFile(path.join(__dirname, '../build/index.html'))
 })
